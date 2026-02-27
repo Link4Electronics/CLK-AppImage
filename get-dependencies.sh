62a2828f49b28d44cef08963578d3fa162ff7cef
Link4Electronics <link4electronics@gmail.com>
2026-02-27 05:46:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+	libglvnd       \
     qt6-base       \
     qt5-multimedia
 
